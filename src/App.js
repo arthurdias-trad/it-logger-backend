bafcf7849c6e818d977bb6c4a4b74d44a12dc6ef
Arthur Dias <arthurdias.trad@outlook.com>
2020-05-27 20:33:50 -0300

    Created logs reducer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useEffect } from "react";
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
 import "./App.css";
+import { Provider } from "react-redux";
+import store from "./store";
 
 import SearchBar from "./components/layout/SearchBar";
 import Logs from "./components/logs/Logs";
@@ -19,17 +21,19 @@ const App = () => {
   }, []);
 
   return (
-    <>
-      <SearchBar />
-      <div className="container">
-        <AddBtn />
-        <AddLogModal />
-        <EditLogModal />
-        <TechListModal />
-        <AddTechModal />
-        <Logs />
-      </div>
-    </>
+    <Provider store={store}>
+      <>
+        <SearchBar />
+        <div className="container">
+          <AddBtn />
+          <AddLogModal />
+          <EditLogModal />
+          <TechListModal />
+          <AddTechModal />
+          <Logs />
+        </div>
+      </>
+    </Provider>
   );
 };
 
