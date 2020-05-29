9b5debe3b1391bd3c9ae2173c2326b1bcaa55922
Arthur Dias <arthurdias.trad@outlook.com>
2020-05-29 15:49:06 -0300

    Added log routes

--- routes/logs.js
+++ routes/logs.js
@@ -1,19 +1,21 @@
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
-  res.send("Get logs from DB");
-});
+router
+  .route("/")
+  .get((req, res) => {
+    res.status(200).send("Get all logs");
+  })
+  .post((req, res) => {
+    res.status(201).send("Add log: " + JSON.stringify(req.body));
+  });
 
-router.post("/", (req, res) => {
-  res.status(201).send("Add log: " + JSON.stringify(req.body));
-});
-
-router.put("/:id", (req, res) => {
-  res.status(201).send("Update log with ID: " + req.params.id);
-});
-
-router.delete("/:id", (req, res) => {
-  res.status(201).send("Delete log with ID: " + req.params.id);
-});
+router
+  .route("/:id")
+  .put((req, res) => {
+    res.status(201).send("Update log with ID: " + req.params.id);
+  })
+  .delete((req, res) => {
+    res.status(201).send("Delete log with ID: " + req.params.id);
+  });
 
 module.exports = router;
